3a5d31dde0337a21147cd6e5084c0b64c9fbc156
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-02-21 00:52:00 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,80 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    email: { type: DataTypes.STRING, unique: true, allowNull: false },
-    passwordHash: { type: DataTypes.STRING, allowNull: false },
-    fullName: DataTypes.STRING,
-    role: { type: DataTypes.STRING, defaultValue: 'client' },
-    bankDetails: DataTypes.JSONB, // { fnbAccount, africanBankAccount }
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('client', 'admin'),
+      defaultValue: 'client'
+    },
+    bankDetails: {
+      type: DataTypes.JSONB,
+      defaultValue: {
+        fnb: '',
+        africanBank: ''
+      }
+    },
+    phone: {
+      type: DataTypes.STRING
+    },
+    company: {
+      type: DataTypes.STRING
+    },
+    vatNumber: {
+      type: DataTypes.STRING
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
+    lastLogin: {
+      type: DataTypes.DATE
+    },
+    metadata: {
+      type: DataTypes.JSONB,
+      defaultValue: {}
+    }
+  }, {
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.passwordHash) {
+          const salt = await bcrypt.genSalt(10);
+          user.passwordHash = await bcrypt.hash(user.passwordHash, salt);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('passwordHash')) {
+          const salt = await bcrypt.genSalt(10);
+          user.passwordHash = await bcrypt.hash(user.passwordHash, salt);
+        }
+      }
+    }
   });
+
+  User.prototype.validatePassword = async function(password) {
+    return bcrypt.compare(password, this.passwordHash);
+  };
+
+  User.associate = function(models) {
+    User.hasMany(models.Project, { foreignKey: 'userId' });
+    User.hasMany(models.Payment, { foreignKey: 'userId' });
+    User.hasMany(models.Payout, { foreignKey: 'userId' });
+  };
+
   return User;
 };
